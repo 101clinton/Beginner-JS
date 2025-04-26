aa70359923d2ec63d970ea0676481c151463e12a
101clinton <hebrewclinton5@gmail.com>
2025-04-26 12:22:01 +0100

    Delete Beiginner JS tasks directory

--- Beiginner JS tasks/Ticket-2/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Varaible Vault
-
-const firstName = "Clinton";
-const age = 25;
-let hubby = "Coding";
-
-console.log(`My name is ${firstName}. I am ${age} years old, I love ${hubby}`)
--- Beiginner JS tasks/Ticket-9/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-let favoriteFoods = ["Rice", "Eru", "Irish", "Yam", "Spaghetti"];
-
-console.log(favoriteFoods[2]);
-favoriteFoods.push("Koki");
-console.log(favoriteFoods);
